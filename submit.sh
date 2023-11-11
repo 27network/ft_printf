4430e60354c16c63a97b95b805954d03c28d8bb8
xtrm <oss@xtrm.me>
2023-11-11 17:59:02 +0100

    ✨ feature(submit.sh): modify submit script

--- submit.sh
+++ submit.sh
@@ -4,16 +4,30 @@
 # Script that removes unwanted elements for submission to the moulinette
 #
 
+# Recreates all submodules as standalone files
+SUBMODULES=$(git config --file .gitmodules --get-regexp path | awk '{ print $2 }')
+for SUBMODULE in $SUBMODULES; do
+	# Get submodule info 
+	SUBMODULE_COMMIT=$(git config --file .gitmodules --get "submodule.$SUBMODULE.commit")
+	SUBMODULE_URL=$(git config --file .gitmodules --get "submodule.$SUBMODULE.url")
+	# Deinit submodule
+	git submodule deinit -f $SUBMODULE
+	git rm --cached $SUBMODULE
+	rm -rf $SUBMODULE
+	# Clone back the submodule as a singular repository
+	git clone $SUBMODULE_URL $SUBMODULE
+	# Checkout the submodule at the specific commit
+	cd $SUBMODULE
+	git checkout $SUBMODULE_COMMIT
+	cd ..
+done
+
 # Remove git files
 find . -name ".git" -exec rm -rf {} \;
 find . -name ".gitignore" -type f -exec rm -rf {} \;
 find . -name ".gitmodules" -type f -exec rm -rf {} \;
 find . -name ".gitattributes" -type f -exec rm -rf {} \;
 find . -name ".git-blame-ignore-revs" -type f -exec rm -rf {} \;
-
-# Libft-related
-## Remove libft tests/ directory
-rm -rf libft/tests/
 
 # If a Makefile exists, run fclean
 if [ -f "Makefile" ]; then
