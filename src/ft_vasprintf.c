395cf1cfaa8f918ec9031e5f3be62318480a2e0f
xtrm <oss@xtrm.me>
2023-11-11 08:00:07 +0100

    ♻️ refactor(ft_vasprintf.c): variable names

--- src/ft_vasprintf.c
+++ src/ft_vasprintf.c
@@ -6,7 +6,7 @@
 /*   By: kiroussa <oss@xtrm.me>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/26 14:26:12 by kiroussa          #+#    #+#             */
-/*   Updated: 2023/11/11 07:34:21 by kiroussa         ###   ########.fr       */
+/*   Updated: 2023/11/11 07:59:35 by kiroussa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -54,15 +54,15 @@ static int	ft_copyback(
 		char **str_ptr,
 		const char *fmt,
 		int current_idx,
-		int *last_nofmt_idx
+		int *last_i
 ) {
 	char	*tmp;
 	int		length;
 
-	if (*last_nofmt_idx == -1)
+	if (*last_i == -1)
 		return (0);
-	tmp = ft_substr(fmt, *last_nofmt_idx, current_idx - *last_nofmt_idx);
-	*last_nofmt_idx = -1;
+	tmp = ft_substr(fmt, *last_i, current_idx - *last_i);
+	*last_i = -1;
 	if (!tmp)
 		return (0);
 	ft_append(str_ptr, tmp, ft_strlen(tmp));
@@ -75,27 +75,27 @@ int	ft_vasprintf(char **str_ptr, const char *fmt, va_list args)
 {
 	int	len;
 	int	i;
-	int	last_nofmt_idx;
+	int	last_i;
 
 	if (!fmt)
 		return (-1);
 	len = 0;
 	i = 0;
-	last_nofmt_idx = -1;
+	last_i = -1;
 	*str_ptr = NULL;
 	while (fmt[i])
 	{
 		if (fmt[i] == *PF_FORMAT_SYMBOL)
-			len += ft_copyback(str_ptr, fmt, i, &last_nofmt_idx);
+			len += ft_copyback(str_ptr, fmt, i, &last_i);
 		if (fmt[i] == *PF_FORMAT_SYMBOL)
 			len += ft_handle_spec(str_ptr, &fmt[i] + 1, &i, args);
 		else
 		{
-			if (last_nofmt_idx == -1)
-				last_nofmt_idx = i;
+			if (last_i == -1)
+				last_i = i;
 			i++;
 		}
 	}
-	len += ft_copyback(str_ptr, fmt, i, &last_nofmt_idx);
+	len += ft_copyback(str_ptr, fmt, i, &last_i);
 	return (len);
 }
